a88986f295a9a244829092688133e49ad86cfa3f
adiandoyo32 <akhiang32@gmail.com>
2022-05-13 02:26:09 +0700

    fix: card ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,86 +2,80 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { LocationCard } from "./components/LocationCard";
 import useGeoLocation from "./hooks/useGeoLocation";
-import DailyForecast from "./models/DailyForecast";
 import ForecastInformation from "./models/ForecastInformation";
 import Location from "./models/Location";
-import WeatherLocation from "./models/WeatherLocation";
 import { getCityByName, getGeoPosition } from "./service/location-service";
 import { getForecastLocation } from "./service/weather-service";
-import {
-  Box,
-  Center,
-  Container,
-  Input,
-  InputGroup,
-  InputRightElement,
-} from "@chakra-ui/react";
+import { Box, Container, Input, InputGroup, InputRightElement, ListItem, UnorderedList } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
 function App() {
-  const [cityName, setCityName] = useState<string>("");
-  const [location, setLocation] = useState<Location | null>(null);
-  const [forecastInformation, setForecastInformation] =
-    useState<ForecastInformation | null>(null);
-  const geoLocation = useGeoLocation();
+    const [cityName, setCityName] = useState<string>("");
+    const [location, setLocation] = useState<Location | null>(null);
+    const [forecastInformation, setForecastInformation] = useState<ForecastInformation | null>(null);
+    const geoLocation = useGeoLocation();
 
-  const onCityNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCityName(e.target.value);
-  };
+    const onCityNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCityName(e.target.value);
+    };
 
-  const onSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      getCityByName(cityName).then((res) => {
-        console.log(res);
-        if (res.length > 0) {
-          setLocation(() => res[0]);
-          console.log(res[0].Key);
-          getForecastLocation(location!.Key).then((res) =>
-            setForecastInformation(res)
-          );
+    const onSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            getCityByName(cityName).then((res) => {
+                console.log(res);
+                if (res.length > 0) {
+                    setLocation(() => res[0]);
+                    console.log(res[0].Key);
+                    getForecastLocation(location!.Key).then((res) => setForecastInformation(res));
+                }
+            });
         }
-      });
-    }
-  };
+    };
 
-  useEffect(() => {
-    if (geoLocation.coords) {
-      getGeoPosition(
-        geoLocation.coords.latitude,
-        geoLocation.coords.longitude
-      ).then((res) => {
-        setLocation(res);
-      });
-    }
-  }, [geoLocation]);
+    useEffect(() => {
+        if (geoLocation.coords) {
+            // getGeoPosition(
+            //   geoLocation.coords.latitude,
+            //   geoLocation.coords.longitude
+            // ).then((res) => {
+            //   setLocation(res);
+            // });
+        }
+    }, [geoLocation]);
 
-  return (
-    <div className="App">
-      <Container>
-        <Box maxW="sm">
-          <InputGroup>
-            <Input
-              type="text"
-              value={cityName}
-              onKeyDown={onSearch}
-              onChange={onCityNameChange}
-              placeholder="Type city name..."
-              size="md"
-            />
-            <InputRightElement
-              pointerEvents="none"
-              children={<SearchIcon color="gray.300" />}
-            />
-          </InputGroup>
+    return (
+        <div className="App">
+            <Container maxW="container.sm">
+                <Box mt="10">
+                    <InputGroup>
+                        <Input
+                            type="text"
+                            value={cityName}
+                            onKeyDown={onSearch}
+                            onChange={onCityNameChange}
+                            placeholder="Type city name..."
+                            size="md"
+                            autoComplete="off"
+                            name="city-name"
+                        />
+                        <InputRightElement pointerEvents="none" children={<SearchIcon color="gray.300" />} />
+                    </InputGroup>
 
-          {location && (
-            <LocationCard location={location} forecast={forecastInformation} />
-          )}
-          {!location && <div>Not found</div>}
-        </Box>
-      </Container>
-    </div>
-  );
+                    <Box borderWidth="1px">
+                        <UnorderedList>
+                            <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                            <ListItem>Consectetur adipiscing elit</ListItem>
+                            <ListItem>Integer molestie lorem at massa</ListItem>
+                            <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                        </UnorderedList>
+                    </Box>
+
+                    {location && <LocationCard location={location} forecast={forecastInformation} />}
+                    {!location && <div>Not found</div>}
+                </Box>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -1,87 +1,102 @@
-import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Image, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { WEATHER_IMAGE_URL } from "../constants/constant";
 import ForecastInformation from "../models/ForecastInformation";
 import Location from "../models/Location";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTemperatureHalf } from "@fortawesome/free-solid-svg-icons";
 
 interface LocationCardProps {
-  location: Location;
-  forecast: ForecastInformation | null;
+    location: Location;
+    forecast: ForecastInformation | null;
 }
 
 export const LocationCard: React.FC<LocationCardProps> = (props) => {
-  const resolveWeatherImage = (iconType: number) => {
-    const icon = iconType.toString().padStart(2, "0");
-    return `${WEATHER_IMAGE_URL}/${icon}-s.png`;
-  };
+    const resolveWeatherImage = (iconType: number) => {
+        const icon = iconType.toString().padStart(2, "0");
+        return `${WEATHER_IMAGE_URL}/${icon}-s.png`;
+    };
 
-  return (
-    <div>
-      <Box>
-        <Box mt="1" fontWeight="bold" lineHeight="tight" isTruncated>
-          <Text fontSize="3xl">{props.location.LocalizedName}</Text>
-        </Box>
-        <Box mt="1" fontWeight="normal" isTruncated>
-          <Text fontSize="md" color="gray.500">
-            {props.location.AdministrativeArea.LocalizedName}
-          </Text>
-        </Box>
-      </Box>
+    return (
+        <Box mt="8" borderWidth="1px" p="8" borderRadius="lg">
+            <Box>
+                <Box mt="1" fontWeight="bold" lineHeight="tight" isTruncated>
+                    <Text fontSize="2xl">{props.location.LocalizedName}</Text>
+                </Box>
+                <Box fontWeight="normal" isTruncated>
+                    <Text fontSize="lg" color="gray.400">
+                        {`${props.location.AdministrativeArea.LocalizedName}, ${props.location.Country.LocalizedName}`}
+                    </Text>
+                </Box>
+            </Box>
 
-      {props.forecast ? (
-        <Box>
-          <Box mt="6" fontWeight="medium" lineHeight="tight">
-            <Text fontSize="lg" color="gray.600">
-              {props.forecast.Headline.Text}
-            </Text>
-          </Box>
-          <Box mt="6" fontWeight="medium" lineHeight="tight">
-            <Text fontSize="lg" color="gray.600">
-              Temperature
-            </Text>
-            <Stack spacing={8} direction="row">
-              <Text fontSize="2xl">
-                {props.forecast.DailyForecasts[0].Temperature.Minimum.Value}
-                {props.forecast.DailyForecasts[0].Temperature.Minimum.Unit}
-              </Text>
-              <Text fontSize="2xl">
-                {props.forecast.DailyForecasts[0].Temperature.Maximum.Value}
-                {props.forecast.DailyForecasts[0].Temperature.Maximum.Unit}
-              </Text>
-            </Stack>
-          </Box>
-          <Box mt="6" fontWeight="medium" lineHeight="tight">
-            <Text fontSize="lg" color="gray.600">
-              Day
-            </Text>
-            <Stack spacing={8} direction="row">
-              <Text fontSize="2xl">
-                {props.forecast.DailyForecasts[0].Day.IconPhrase}
-              </Text>
-              <Image
-                src={resolveWeatherImage(
-                  props.forecast.DailyForecasts[0].Day.Icon
-                )}
-              />
-            </Stack>
-            <Text fontSize="lg" color="gray.600">
-              Night
-            </Text>
-            <Stack spacing={8} direction="row">
-              <Text fontSize="2xl">
-                {props.forecast.DailyForecasts[0].Night.IconPhrase}
-              </Text>
-              <Image
-                src={resolveWeatherImage(
-                  props.forecast.DailyForecasts[0].Night.Icon
-                )}
-              />
-            </Stack>
-          </Box>
+            {props.forecast ? (
+                <Box mt="8">
+                    <Text fontSize="md" color="gray.600">
+                        {props.forecast.Headline.Text}
+                    </Text>
+                    <Box mt="8" fontWeight="medium" lineHeight="tight">
+                        <Grid templateColumns="repeat(2, 1fr)">
+                            <GridItem w="100%">
+                                <Box fontWeight="medium" lineHeight="tight">
+                                    <Stack alignItems="center" spacing="0">
+                                        <Text fontSize="md" color="gray.600">
+                                            Day
+                                        </Text>
+                                        <Text fontSize="xl">{props.forecast.DailyForecasts[0].Day.IconPhrase}</Text>
+                                        <Image
+                                            width="100%"
+                                            boxSize="80px"
+                                            objectFit="contain"
+                                            src={resolveWeatherImage(props.forecast.DailyForecasts[0].Day.Icon)}
+                                        />
+                                    </Stack>
+                                </Box>
+                            </GridItem>
+                            <GridItem w="100%">
+                                <Box fontWeight="medium" lineHeight="tight">
+                                    <Stack alignItems="center" spacing="0">
+                                        <Text fontSize="md" color="gray.600">
+                                            Night
+                                        </Text>
+                                        <Text fontSize="xl">{props.forecast.DailyForecasts[0].Night.IconPhrase}</Text>
+                                        <Image
+                                            width="100%"
+                                            boxSize="80px"
+                                            objectFit="contain"
+                                            src={resolveWeatherImage(props.forecast.DailyForecasts[0].Night.Icon)}
+                                        />
+                                    </Stack>
+                                </Box>
+                            </GridItem>
+                        </Grid>
+                    </Box>
+
+                    <Box mt="6">
+                        <Stack spacing={4} direction="row" justifyItems="center" alignItems="center">
+                            <Text fontSize="lg" color="gray.600">
+                                <FontAwesomeIcon size="2x" icon={faTemperatureHalf} />
+                            </Text>
+                            <Stack>
+                                <Box fontWeight="bold" lineHeight="tight">
+                                    <Text fontSize="xl">
+                                        {props.forecast.DailyForecasts[0].Temperature.Minimum.Value}
+                                        {props.forecast.DailyForecasts[0].Temperature.Minimum.Unit}
+                                    </Text>
+                                </Box>
+                                <Box fontWeight="bold" lineHeight="tight">
+                                    <Text fontSize="xl">
+                                        {props.forecast.DailyForecasts[0].Temperature.Maximum.Value}
+                                        {props.forecast.DailyForecasts[0].Temperature.Maximum.Unit}
+                                    </Text>
+                                </Box>
+                            </Stack>
+                        </Stack>
+                    </Box>
+                </Box>
+            ) : (
+                <div>asdasd</div>
+            )}
         </Box>
-      ) : (
-        <div>asdasd</div>
-      )}
-    </div>
-  );
+    );
 };
