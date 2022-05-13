780e4b0a6c2fc201f0abc9960497ad449acd3a04
adiandoyo.dl32 <adiandoyo.dl32@gmail.com>
2022-05-13 13:45:52 +0700

    fix: change api to https

--- src/service/location-service.ts
+++ src/service/location-service.ts
@@ -4,7 +4,7 @@ export const getGeoPosition = (lat: number, lng: number) => {
   const apiKey: string = import.meta.env.VITE_APP_WEATHER_API_KEY;
   const q = `${lat},${lng}`;
   return fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?q=${q}&apikey=${apiKey}`
+    `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?q=${q}&apikey=${apiKey}`
   )
     .then((res) => res.json())
     .then((data) => data as Location);
@@ -14,7 +14,7 @@ export const getCityByName = (cityName: string) => {
   const apiKey: string = import.meta.env.VITE_APP_WEATHER_API_KEY;
   const q = cityName
   return fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/search?q=${q}&apikey=${apiKey}`
+    `https://dataservice.accuweather.com/locations/v1/cities/search?q=${q}&apikey=${apiKey}`
   )
     .then((res) => res.json())
     .then((data) => data as Location[]);
--- src/service/weather-service.ts
+++ src/service/weather-service.ts
@@ -3,7 +3,7 @@ import ForecastInformation from "../models/ForecastInformation";
 export const getForecastLocation = (locationKey: string) => {
   const apiKey = import.meta.env.VITE_APP_WEATHER_API_KEY;
   return fetch(
-    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?details=true&apikey=${apiKey}`
+    `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?details=true&apikey=${apiKey}`
   )
     .then((res) => res.json())
     .then((data) => data as ForecastInformation);
